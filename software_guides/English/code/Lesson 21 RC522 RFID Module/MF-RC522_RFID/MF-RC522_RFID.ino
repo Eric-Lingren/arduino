918edd0413859a1a2d8b9757e5f116a30f9561c9
Eric-Lingren <44101756+Eric-Lingren@users.noreply.github.com>
2019-02-15 00:39:47 -0700

    adding test file and configuring vscode to work with arduino

--- software_guides/English/code/Lesson 21 RC522 RFID Module/MF-RC522_RFID/MF-RC522_RFID.ino
+++ software_guides/English/code/Lesson 21 RC522 RFID Module/MF-RC522_RFID/MF-RC522_RFID.ino
@@ -73,38 +73,46 @@ void loop() {
   // Now a card is selected. The UID and SAK is in mfrc522.uid.
   
   // Dump UID
+   Serial.print("uid size: "  );
+  Serial.print(mfrc522.uid.size );
+  
+  Serial.print("\r\n");
   Serial.print(F("Card UID:"));
+ 
   for (byte i = 0; i < mfrc522.uid.size; i++) {
+     
     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
     Serial.print(mfrc522.uid.uidByte[i], HEX);
   } 
+  
   Serial.println();
+ 
 
   // Dump PICC type
-//  MFRC522::PICC_Type piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
-//  Serial.print(F("PICC type: "));
-//  Serial.print(mfrc522.PICC_GetTypeName(piccType));
-//  Serial.print(F(" (SAK "));
-//  Serial.print(mfrc522.uid.sak);
-//  Serial.print(")\r\n");
-//  if (  piccType != MFRC522::PICC_TYPE_MIFARE_MINI 
-//    &&  piccType != MFRC522::PICC_TYPE_MIFARE_1K
-//    &&  piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
-//    Serial.println(F("This sample only works with MIFARE Classic cards."));
-//    return;
-//  }
-  
-  // Set new UID
-  byte newUid[] = NEW_UID;
-  if ( mfrc522.MIFARE_SetUid(newUid, (byte)4, true) ) {
-    Serial.println(F("Wrote new UID to card."));
-  }
-  
-  // Halt PICC and re-select it so DumpToSerial doesn't get confused
-  mfrc522.PICC_HaltA();
-  if ( ! mfrc522.PICC_IsNewCardPresent() || ! mfrc522.PICC_ReadCardSerial() ) {
+  MFRC522::PICC_Type piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
+  Serial.print(F("PICC type: "));
+  Serial.print(mfrc522.PICC_GetTypeName(piccType));
+  Serial.print(F(" (SAK "));
+  Serial.print(mfrc522.uid.sak);
+  Serial.print(")\r\n");
+  if (  piccType != MFRC522::PICC_TYPE_MIFARE_MINI 
+    &&  piccType != MFRC522::PICC_TYPE_MIFARE_1K
+    &&  piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
+    Serial.println(F("This sample only works with MIFARE Classic cards."));
     return;
   }
+  
+//   Set new UID
+   byte newUid[] = NEW_UID;
+   if ( mfrc522.MIFARE_SetUid(newUid, (byte)4, true) ) {
+     Serial.println(F("Wrote new UID to card."));
+   }
+  
+  // Halt PICC and re-select it so DumpToSerial doesn't get confused
+   mfrc522.PICC_HaltA();
+   if ( ! mfrc522.PICC_IsNewCardPresent() || ! mfrc522.PICC_ReadCardSerial() ) {
+     return;
+   }
   
   // Dump the new memory contents
   Serial.println(F("New UID and contents:"));
